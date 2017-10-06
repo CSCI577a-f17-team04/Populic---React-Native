c472a5648ef422e68c258f1d3b389c03c06d7e0e
coder1216 <alexhong911216@gmail.com>
2017-10-05 19:57:58 -0700

    Add files via upload
    Update gameIsOn interface, gameStart interface and Challenge interface.
    Bugs fixed.
    UI modify.

--- Challenge.js
+++ Challenge.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-
-
-import { View, Text, Image, Alert,Button, AsyncStorage,StyleSheet, Text as TextReact, Share, Modal, Keyboard, Easing, FlatList, ScrollView, Dimensions, InteractionManager, TouchableOpacity, TouchableWithoutFeedback, TouchableHighlight} from 'react-native';
+import { View,
+  Text,
+  Image,
+  Alert,
+  Button,
+  AsyncStorage,
+  StyleSheet,
+  Text as TextReact,
+  Share,
+  Modal,
+  Keyboard,
+  Easing,
+  FlatList,
+  ScrollView,
+  Dimensions,
+  InteractionManager,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TouchableHighlight} from 'react-native';
 import one from '../../views/challenge/Suggestion';
 import {Navigator} from 'react-native-deprecated-custom-components'
 
 var Challenge = React.createClass({
-
+                                  
     getInitialState: function () {
         return { checked : this.props.initClick};
     },
-
 
     showNewsDetailView() {
         console.log('点击cell')
@@ -38,39 +53,37 @@ var Challenge = React.createClass({
                     <Text style = {styles.captionText}>Give a hug to random person</Text>
                 </View>
 
-                <View style={styles.challengeButton} >
-                    <Image style = {styles.captionImage } source={require('../../images/pin.png')}/>
-                    <TouchableOpacity   onPress={() => {this.props.callbackParent(); navigate('Camera', {name: 'Test'}); } }>
-                        <Text style={styles.btnText}> do it</Text>
-                    </TouchableOpacity>
-
-                </View>
+                 <View style={styles.challengeStatue} >
+                     <Text style = {styles.challengeStatueText}>You don't have challenge partner</Text>
+                 </View>
 
                 <View style={styles.challengeButton} >
                     <Image style = {styles.captionImage } source={require('../../images/pin.png')}/>
-                    <TouchableOpacity   >
-                        <Text style={styles.btnText}> Challenge your friend</Text>
+                    <TouchableOpacity >
+                        <Text style={styles.btnText}> Challenge your friends</Text>
                     </TouchableOpacity>
 
                 </View>
 
 
                 <View  style={styles.upChallengeContentCaption}>
-                    <Text style={styles.captionText }> upcoming challenge</Text>
+                    <Text style={styles.captionText }> Upcoming Challenge</Text>
                 </View>
+               
                 <View style={styles.upComingChallenge} >
 
                    <View style={styles.upChallengeContent}>
-                        <Text style={styles.textProperty }> Eat banana in 7 second</Text>
+                        <Text style={styles.textProperty }> Eat banana in 8 second</Text>
                        <Text style={styles.textProperty }> shotgun a beer</Text>
                    </View>
 
                     <View style={styles.upChallengeContent}>
-                        <Text style={styles.textProperty }> Eat banana in 7 second</Text>
+                        <Text style={styles.textProperty }> Eat banana in 21 second</Text>
                         <Text style={ styles.textProperty }> shotgun a beer</Text>
                     </View>
 
                 </View>
+               
                 <View style={styles.challengeButton} >
                     <Image style = {styles.captionImage } source={require('../../images/pin.png')}/>
                     <TouchableOpacity   >
@@ -83,10 +96,6 @@ var Challenge = React.createClass({
 
 
             </View>
-
-
-
-
 
         );
     },
@@ -111,7 +120,20 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         alignItems:'center',
         justifyContent:"center",
+        top: 40,
 
+    },
+    challengeStatue:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:"center",
+    },
+    challengeStatueText:{
+        fontSize: 15,
+        color:'#83E027',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:"center",
     },
     buttonStyle:{
         borderWidth:3,
@@ -132,6 +154,7 @@ const styles = StyleSheet.create({
         color:'white'
     },
     upChallengeContent:{
+        top: 40,
         flexDirection:'column',
     },
     upComingChallenge:{
@@ -142,20 +165,27 @@ const styles = StyleSheet.create({
         margin:10,
     },
     challengeButton:{
-        backgroundColor:"#5CACEE",
+        backgroundColor:"#277DE0",
         flexDirection:'row',
         alignItems:'center',
         justifyContent:"center",
         margin:10,
-        borderWidth:3,
+        borderWidth:2,
         borderColor:"white",
         borderRadius:5,
+        padding:8,
+        top: 40,
+
+    },
+    btnText:{
+        color:"white",
     },
     challengeContent:{
         flexDirection:'row',
         alignItems:'center',
         justifyContent:"center",
         margin:10,
+
     },
     timeLeft:{
         alignItems:"flex-end",
@@ -175,14 +205,16 @@ const styles = StyleSheet.create({
     },
     popScreen:{
         position:"relative",
-        backgroundColor: "#84c1ff",
+        backgroundColor: "#4AA0DF",
         width: Dimensions.get('window').width-(Dimensions.get('window').width)/7,
         height: Dimensions.get('window').height-(Dimensions.get('window').height)/7,
         left:(Dimensions.get('window').width)/14,
         flexDirection:'column',
         padding:30,
         borderColor: "white",
-        borderWidth:3
+        borderWidth:3,
+                                 top:30,
+                                 zIndex: -1
     }
 });
 module.exports = Challenge;
