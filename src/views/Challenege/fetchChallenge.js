b0d24c721516f75298b050a36c036d90d2180631
coder1216 <yufeihon@usc.edu>
2017-11-04 23:22:29 -0700

    Build APIs by self [related to JIRA POP-97]
    Build APIs for every members to fetch JSON from server easily
    [related to JIRA POP-97]

--- src/views/Challenege/fetchChallenge.js
+++ src/views/Challenege/fetchChallenge.js
@@ -33,6 +33,16 @@ export const FetchChallenge = function(dataType){
             request = generateRequest(challenegAPI.isMatched);
             break;
 
+        // case challenegAPI.addPost:
+        //     url = `${SERVER}/${challenegAPI.addPost}`;
+        //     request = generateRequest(challenegAPI.addPost);
+        //     break;
+
+        // case challenegAPI.getImage:
+        //     url = `${SERVER}/${challenegAPI.getImage}`;
+        //     request = generateRequest(challenegAPI.getImage);
+        //     break;
+
         case challenegAPI.competitorCandidates:
             url = `${SERVER}/${challenegAPI.competitorCandidates}`;
             request = generateRequest(challenegAPI.competitorCandidates);
@@ -108,6 +118,71 @@ var generateRequest = ( dataType, input1="test2", input2, input3 ) =>{
             };
             return request;
             break;
+
+            
+        // case challenegAPI.isMatched:
+        //     var request = {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },body: JSON.stringify({
+        //             username: "test2",
+        //         })
+        //         .then((response) => response.json())
+        //         .then((responseJson) => {
+        //             this.setState({
+        //               username: responseJson[0].username,
+        //               me: responseJson[0].nickname,
+        //               competitor: responseJson[1].nickname
+        //             });
+        //             if(this.state.username == "test1"){
+        //               this.setState ({me: this.state.left});
+        //               this.setState ({competitor: this.state.right});
+        //            }else{
+        //               this.setState ({me: this.state.right});
+        //               this.setState ({competitor: this.state.left});
+        //            }
+        //         })
+        //         .catch(error => {
+        //             this.setState({ error, loading: false });
+        //         })
+        //     };
+        //     return request;
+        //     break;
+
+
+            // case challenegAPI.addPost:
+            // var request = {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },body: JSON.stringify({
+            //         username: "test2",
+            //         img: this.state.imgURL,
+            //         date: "10-20-2017",
+            //         isChallenge: "true",
+            //     })
+            // };
+            // return request;
+            // break;
+
+
+
+            
+        // case challenegAPI.getImage:
+        //     var request = {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },body: JSON.stringify({
+
+        //             username: "test3",
+        //             date: "10-20-2017",
+        //         })
+        //     };
+        //     return request;
+        //     break;
+
 
         case challenegAPI.competitorCandidates:
             var request = {
